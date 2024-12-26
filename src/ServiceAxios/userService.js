10f4bad4bb8c4ef77f76f7c00a81321181ae02a6
Duc PC <fishjiro@gmail.com>
2024-12-26 21:53:43 +0700

    R with pageginate

--- src/ServiceAxios/userService.js
+++ src/ServiceAxios/userService.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 const createUser = async (email, username, phone, password) => {
-    return await axios.post("http://localhost:9000/api/signup", { email, username, phone, password })
+    return await axios.post("http://localhost:9000/api/signup", { email, username, phone, password });
 }
 const loginUser = async (email, password) => {
-    return await axios.post("http://localhost:9000/api/login", { email, password })
+    return await axios.post("http://localhost:9000/api/login", { email, password });
+}
+const readUsers = async () => {
+    return await axios.get("http://localhost:9000/api/users");
+}
+const readUsersWithPage = async (page) => {
+    return await axios.get(`http://localhost:9000/api/userspage?page=${page}`);
 }
 export {
-    createUser, loginUser
+    createUser, loginUser, readUsers, readUsersWithPage
 }
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,8 +1,98 @@
+import { useEffect, useState } from "react";
+import { readUsers, readUsersWithPage } from "../../ServiceAxios/userService";
+import Pagination from 'react-bootstrap/Pagination';
+import "./User.scss"
+import Form from 'react-bootstrap/Form';
+
 const User = () => {
+    const [listUsers, setListUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPage, setTotalPage] = useState(10);
+    useEffect(() => {
+        getUsers();
+    }, [currentPage])
+
+    let response;
+    let responseData;
+    const getUsers = async () => {
+        try {
+            response = await readUsersWithPage(currentPage);
+            console.log("check response", response);
+            if (response) {
+                responseData = response.data;
+                if (+responseData.EC === 0) {
+                    setListUsers(responseData.DT.data);
+                    console.log("checkTotalPage", responseData.DT.totalPages);
+                    setTotalPage(responseData.DT.totalPages);
+                }
+            }
+        } catch (error) {
+
+        }
+    }
+    // const setPage = (page) => {
+    //     setCurrentPage({ page });
+    // }
+
+    const changePage = (page) => {
+        setCurrentPage(page);
+    }
     return (
-        <div>
-            Hello User
-        </div>
+        <div className="User-container container">
+            <table className="table table-hover table-bordered">
+                <thead>
+                    <tr className="d-flex">
+                        <th style={{ flex: 2 }}>Username</th>
+                        <th style={{ flex: 3 }}>Email</th>
+                        <th style={{ flex: 3 }}>Address</th>
+                        <th style={{ flex: 2 }}>Phone</th>
+                        <th style={{ flex: 2 }}>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        listUsers && listUsers.length > 0 ?
+                            <>
+                                {listUsers.map((item, index) => {
+                                    return (
+                                        <tr className="d-flex" key={index}>
+                                            <td style={{ flex: 2 }}>{item.username}</td>
+                                            <td style={{ flex: 3 }}>{item.email}</td>
+                                            <td style={{ flex: 3 }}>{item.address}</td>
+                                            <td style={{ flex: 2 }}>{item.phone}</td>
+                                            <td style={{ flex: 2 }}>{item.Role ? item.Role.roleName : "null"}</td>
+                                        </tr>
+                                    )
+
+                                })}
+                            </>
+                            :
+                            <>
+                                <tr>
+                                    <td>
+                                        Không có bản ghi nào
+                                    </td>
+                                </tr>
+                            </>
+                    }
+                </tbody>
+
+            </table>
+            <span className="d-flex" style={{ justifyContent: 'center', alignItems: 'center' }}>
+                Trang hiện tại:
+                <span>
+                    <Form.Select onChange={(event) => changePage(Number(event.target.value))}>
+                        {
+                            totalPage && Array.from({ length: totalPage }, (index, value) => value + 1).map((value, index) => (
+
+                                <option key={index} value={value} >{value}</option>
+                            ))
+
+                        }
+                    </Form.Select>
+                </span>
+            </span>
+        </div >
     )
 }
 export default User;
